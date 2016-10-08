32a26e28ed15063a64deb25409c22646a401d2a9
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2016-10-08 15:24:36 +0200

    SVR-17: Removed old quotes to execute expect block as a String.

--- mysql/mysql-secure-installation.sh
+++ mysql/mysql-secure-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# forked from https://gist.github.com/coderua/5592d95970038944d099
+# Forked from https://gist.github.com/coderua/5592d95970038944d099
 #
 # Automate mysql secure installation for RedHat based systems
 # 
@@ -72,29 +72,29 @@ fi
 set timeout 3
 spawn mysql_secure_installation
 
-expect \"Enter current password for root (enter for none):\"
-send \"$CURRENT_MYSQL_PASSWORD\r\"
+expect "Enter current password for root (enter for none):"
+send "$CURRENT_MYSQL_PASSWORD\r"
 
-expect \"root password?\"
-send \"y\r\"
+expect "root password?"
+send "y\r"
 
-expect \"New password:\"
-send \"$NEW_MYSQL_PASSWORD\r\"
+expect "New password:"
+send "$NEW_MYSQL_PASSWORD\r"
 
-expect \"Re-enter new password:\"
-send \"$NEW_MYSQL_PASSWORD\r\"
+expect "Re-enter new password:"
+send "$NEW_MYSQL_PASSWORD\r"
 
-expect \"Remove anonymous users?\"
-send \"y\r\"
+expect "Remove anonymous users?"
+send "y\r"
 
-expect \"Disallow root login remotely?\"
-send \"y\r\"
+expect "Disallow root login remotely?"
+send "y\r"
 
-expect \"Remove test database and access to it?\"
-send \"y\r\"
+expect "Remove test database and access to it?"
+send "y\r"
 
-expect \"Reload privilege tables now?\"
-send \"y\r\"
+expect "Reload privilege tables now?"
+send "y\r"
 
 EOF
 
